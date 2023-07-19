5325777d6230ff1505aff08c65cd08be0cc92eca
Sengoku-rem <103261281+Sengoku-rem@users.noreply.github.com>
2023-07-19 16:11:45 +0900

    5th commit

--- scripts/musicBot.js
+++ scripts/musicBot.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const { spawn } = require('child_process');
 const ffmpegPath = require('ffmpeg-static');
+const fs = require('fs');
+
+const { google } = require('googleapis');
+const privatekey = require('./privatekey.json');
 
 // 10進数を12進数に変換する関数
 function convertDecimalToHex(decimalValue) {
@@ -18,12 +22,38 @@ function convertHexToFile(hexValue) {
   }
 }
 
+// 12進数から音名に変換する関数
+function convertHexToNote(hexValue) {
+  const noteMap = {
+    0: 'ラ',
+    1: 'ラ#',
+    2: 'シ',
+    3: 'ド',
+    4: 'ド#',
+    5: 'レ',
+    6: 'レ#',
+    7: 'ミ',
+    8: 'ファ',
+    9: 'ファ#',
+    a: 'ソ',
+    b: 'ソ#',
+  };
+  let notes = '';
+  for (let i = 0; i < hexValue.length; i++) {
+    const note = noteMap[hexValue[i]];
+    notes += note + ' ';
+  }
+  return notes.trim();
+}
+
 // 音声ファイルを結合する関数
-function concatAudio (inputFiles, outputFilePath) {
+function concatAudio(inputFiles, outputFilePath) {
   const ffmpeg = spawn(ffmpegPath, [
-    '-i', `concat:${inputFiles.join('|')}`,
-    '-c', 'copy',
-    outputFilePath
+    '-i',
+    `concat:${inputFiles.join('|')}`,
+    '-c',
+    'copy',
+    outputFilePath,
   ]);
 
   ffmpeg.stdout.on('data', (data) => {
@@ -42,6 +72,11 @@ function concatAudio (inputFiles, outputFilePath) {
 module.exports = (robot) => {
   robot.respond(/music:(.+)/i, async (res) => {
     let inputDecimal = parseFloat(res.match[1]);
+    if (!Number.isInteger(inputDecimal)) {
+      res.send('コマンドは music:整数 です');
+      return;
+    }
+
     let hexValue = convertDecimalToHex(inputDecimal);
 
     console.log(`[Input] Decimal: ${inputDecimal}`);
@@ -66,13 +101,100 @@ module.exports = (robot) => {
     // 音声ファイルを結合
     concatAudio(filePaths, outputFilePath);
 
-
-    res.send(`12進数に直すと` + hexValue);
+    const noteText = convertHexToNote(hexValue);
+    res.send(`12進数に直すと: ${hexValue}`);
+    res.send(`音階に対応させると: ${noteText}`);
     // 結合されたファイルのパスを返す
     res.send({
-      path: outputFilePath
+      path: outputFilePath,
     });
+
+    // Google Drive に保存
+    const drive = google.drive({
+      version: 'v3',
+      auth: new google.auth.GoogleAuth({
+        credentials: privatekey,
+        scopes: ['https://www.googleapis.com/auth/drive'],
+      }),
+    });
+
+    const folderId = '17M6NZ9Zf13NLVf05OhJF5ntj6l9la6gv';
+    const fileMetadata = {
+      name: outputFilename,
+      mimeType: 'audio/mpeg',
+      parents: [folderId],
+    };
+    const media = {
+      mimeType: 'audio/mpeg',
+      body: fs.createReadStream(outputFilePath),
+    };
+
+    // 同じ名前のファイルが存在するかチェック
+    drive.files.list(
+      {
+        q: `'${folderId}' in parents and name='${outputFilename}'`,
+        fields: 'files(name)',
+      },
+      (err, response) => {
+        if (err) {
+          console.error('Error retrieving file:', err);
+        } else {
+          const files = response.data.files;
+          if (files.length > 0) {
+            console.log('File already exists:', outputFilename);
+          } else {
+            drive.files.create(
+              {
+                resource: fileMetadata,
+                media: media,
+                fields: 'id',
+              },
+              (err, file) => {
+                if (err) {
+                  console.error('Error creating file:', err);
+                } else {
+                  console.log('File created:', file);
+                }
+              }
+            );
+          }
+        }
+      }
+    );
   });
 
-  // ...
+  robot.respond(/all/i, async (res) => {
+    // Google Drive から最新の3つの音声ファイルを取得
+    const drive = google.drive({
+      version: 'v3',
+      auth: new google.auth.GoogleAuth({
+        credentials: privatekey,
+        scopes: ['https://www.googleapis.com/auth/drive'],
+      }),
+    });
+
+    const folderId = '17M6NZ9Zf13NLVf05OhJF5ntj6l9la6gv';
+
+    drive.files.list(
+      {
+        q: `'${folderId}' in parents`,
+        fields: 'files(name, createdTime)',
+        orderBy: 'createdTime desc',
+        pageSize: 3,
+      },
+      (err, response) => {
+        if (err) {
+          console.error('Error retrieving files:', err);
+        } else {
+          const files = response.data.files;
+          let fileList = '';
+          files.forEach((file) => {
+            fileList += `${file.name} - ${file.createdTime}\n`;
+          });
+          res.send('最新の3つの音声ファイル:\n' + fileList);
+          res.send('すべての音声ファイルはこちらから:\n https://drive.google.com/drive/folders/17M6NZ9Zf13NLVf05OhJF5ntj6l9la6gv');
+        }
+      }
+    );
+  });
 };
